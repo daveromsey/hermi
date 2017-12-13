1d30d110048efd7365f7b4b018b56e14ac68b2ec
David Romsey <dromsey@gmail.com>
2017-12-13 17:21:30 -0500

    Remove off-canvas-main.php. This is now handled in off-canvas-start.php.

--- template-parts/navigation/foundation-off-canvas/off-canvas-main.php
+++
@@ -1,8 +0,0 @@
-<div class="off-canvas position-left" id="off-canvas" data-off-canvas data-position="left">
-
-	<button class="close-button" aria-label="<?php _e( 'Close menu', 'hermi' ); ?>" type="button" data-close="">
-		<span aria-hidden="true">×</span>
-	</button>
-
-	<?php get_template_part( 'template-parts/navigation/off-canvas/wp-nav-menu' ); ?>
-</div><!-- .off-canvas .position-left -->
