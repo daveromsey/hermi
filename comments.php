46f68050c136316fd051c91b7fdce931f4e572b5
David Romsey <dromsey@gmail.com>
2017-12-14 20:40:47 -0500

    Add grid container.

--- comments.php
+++ comments.php
@@ -23,71 +23,73 @@ if ( post_password_required() ) {
 <?php do_action( 'hermi_comments_before' ); ?>
 <div id="comments" class="entry-comments">
 
-	<div class="grid-x">
-		<div class="small-12 cell">
-			<?php do_action( 'hermi_comments_top' ); ?>
+	<div class="grid-container">
+		<div class="grid-x">
+			<div class="small-12 cell">
+				<?php do_action( 'hermi_comments_top' ); ?>
 
-			<?php if ( have_comments() ) { ?>
-			
-				<h2 class="comments-title">
-					<?php
-						$comments_number = get_comments_number();
-						if ( 1 === $comments_number ) {
-							/* translators: %s: post title */
-							printf( _x( 'One comment on &ldquo;%s&rdquo;', 'comments title', 'hermi' ), get_the_title() );
-						} else {
-							printf(
-								/* translators: 1: number of comments, 2: post title */
-								_nx(
-									'%1$s comment on &ldquo;%2$s&rdquo;',
-									'%1$s comments on &ldquo;%2$s&rdquo;',
-									$comments_number,
-									'comments title',
-									'hermi'
-								),
-								number_format_i18n( $comments_number ),
-								get_the_title()
-							);
-						}
-					?>
-				</h2>
+				<?php if ( have_comments() ) { ?>
+				
+					<h2 class="comments-title">
+						<?php
+							$comments_number = get_comments_number();
+							if ( 1 === $comments_number ) {
+								/* translators: %s: post title */
+								printf( _x( 'One comment on &ldquo;%s&rdquo;', 'comments title', 'hermi' ), get_the_title() );
+							} else {
+								printf(
+									/* translators: 1: number of comments, 2: post title */
+									_nx(
+										'%1$s comment on &ldquo;%2$s&rdquo;',
+										'%1$s comments on &ldquo;%2$s&rdquo;',
+										$comments_number,
+										'comments title',
+										'hermi'
+									),
+									number_format_i18n( $comments_number ),
+									get_the_title()
+								);
+							}
+						?>
+					</h2>
+					
+					<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) { // Are there comments to navigate through? ?>
+						<nav class="comment-pagination nav-pagination comment-nav-top">
+							<?php hermi_comment_nav_links(); ?>
+						</nav>
+					<?php } ?>
+
+					<ol class="commentlist">
+						<?php
+							wp_list_comments( [
+								'callback' => apply_filters( 'hermi_list_comments_callback_name', 'hermi_list_comments' ),
+							] );
+						?>
+					</ol>
+				<?php } ?>
 				
 				<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) { // Are there comments to navigate through? ?>
-					<nav class="comment-pagination nav-pagination comment-nav-top">
+					<nav class="comment-pagination nav-pagination comment-nav-bottom">
 						<?php hermi_comment_nav_links(); ?>
 					</nav>
+				<?php } ?>			
+
+				<?php if ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) { ?>
+					<p class="comments-closed"><?php _e( 'Comments are closed.', 'hermi' ); ?></p>
 				<?php } ?>
 
-				<ol class="commentlist">
-					<?php
-						wp_list_comments( [
-							'callback' => apply_filters( 'hermi_list_comments_callback_name', 'hermi_list_comments' ),
-						] );
-					?>
-				</ol>
-			<?php } ?>
-			
-			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) { // Are there comments to navigate through? ?>
-				<nav class="comment-pagination nav-pagination comment-nav-bottom">
-					<?php hermi_comment_nav_links(); ?>
-				</nav>
-			<?php } ?>			
-
-			<?php if ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) { ?>
-				<p class="comments-closed"><?php _e( 'Comments are closed.', 'hermi' ); ?></p>
-			<?php } ?>
-
-			<?php
-				comment_form( [
-					'title_reply' => __( 'Reply', 'hermi' ),
-					//'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
-					//'title_reply_after'  => '</h2>',					
-				] );
-			?>
-			
-			<?php do_action( 'hermi_comments_bottom' ); ?>
-		</div><!-- .cell .small-12 -->
-	</div><!-- .grid-x -->
+				<?php
+					comment_form( [
+						'title_reply' => __( 'Reply', 'hermi' ),
+						//'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+						//'title_reply_after'  => '</h2>',					
+					] );
+				?>
+				
+				<?php do_action( 'hermi_comments_bottom' ); ?>
+			</div><!-- .cell .small-12 -->
+		</div><!-- .grid-x -->
+	</div><!-- .grid-container -->
 	
 </div><!-- #comments -->
 <?php do_action( 'hermi_comments_after' ); ?>
