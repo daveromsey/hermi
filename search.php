75437ca89d7ef38a66d8e5c81141e5a5ff3dfee9
David Romsey <dromsey@gmail.com>
2017-12-17 20:38:17 -0500

    Add dedicated search.php template to handle search results page.

---
+++ search.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The search results template file.
+ *
+ * @package Hermi
+ * @since Hermi 0.1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+require_once( locate_template( 'templates/tpl-post-archive-grid.php' ) );
+exit();
