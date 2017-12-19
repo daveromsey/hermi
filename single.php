069758c55d2ee92d9a3429198196ead2300986f9
David Romsey <dromsey@gmail.com>
2017-12-19 01:02:04 -0500

    Update layout and template names.

--- single.php
+++ single.php
@@ -16,7 +16,20 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Note that posts allow a custom template to be selected, which 
  * overrides the default template.
  */
-switch ( hermi_get_layout( 'post-single' ) ) :
+switch ( hermi_get_layout( 'single-post' ) ) :
+
+	default : // Failover template
+	case ( 'layout-standard' ) :
+		require_once( locate_template( 'templates/tpl-single-post.php' ) );
+	break;
+
+	case ( 'layout-narrow' ) :
+		require_once( locate_template( 'templates/tpl-single-post-narrow.php' ) );
+	break;
+
+	case ( 'layout-full-width' ) :
+		require_once( locate_template( 'templates/tpl-single-post-full-width.php' ) );
+	break;
 
 	case ( 'layout-content-sidebar' ) :
 		require_once( locate_template( 'templates/tpl-single-post-sidebar-right.php' ) );
@@ -24,18 +37,6 @@ switch ( hermi_get_layout( 'post-single' ) ) :
 
 	case ( 'layout-sidebar-content' ) :
 		require_once( locate_template( 'templates/tpl-single-post-sidebar-left.php' ) );
-	break;
-
-	case ( 'layout-grid' ) :
-		require_once( locate_template( 'templates/tpl-single-post.php' ) );
-	break;
-
-	case ( 'layout-grid-narrow' ) :
-		require_once( locate_template( 'templates/tpl-single-post-narrow.php' ) );
-	break;
-
-	case ( 'layout-full-width' ) :
-		require_once( locate_template( 'templates/tpl-single-post-full-width.php' ) );
 	break;
 
 endswitch;
