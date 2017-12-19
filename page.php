00c754b314c4d588c2867ad7f6853cf5b55ff401
David Romsey <dromsey@gmail.com>
2017-12-19 01:03:29 -0500

    Update template names.

--- page.php
+++ page.php
@@ -12,8 +12,8 @@
 /**
  * Available templates:
  *	templates/
- *   tpl-page-grid.php
- *   tpl-page-grid-narrow.php (default)
+ *   tpl-page.php
+ *   tpl-page-narrow.php (default)
  *   tpl-page-full-width.php
  *   tpl-page-sidebar-right.php
  *   tpl-page-sidebar-left.php
@@ -21,14 +21,14 @@
 
 // Load default template with require_once() because we don't want this to fail silently.  
 require_once( locate_template(
-	apply_filters( 'hermi_default_page_template', 'templates/tpl-page-grid-narrow.php' )
+	apply_filters( 'hermi_default_page_template', 'templates/tpl-page-narrow.php' )
 ) ); 
 
 /* 
 // Example of changing default page template with a filter:
 add_filter( 'hermi_default_page_template', 'hermi_change_default_page_template' );
 function hermi_change_default_page_template( $page_template ) {
-	return 'tpl-page-grid.php';
+	return 'tpl-page.php';
 }
 */
 
