1f3764f1973632934fddafdf2d1a181ff5e944ec
David Romsey <dromsey@gmail.com>
2017-12-20 18:49:03 -0500

    Minor updates to inline docs.

--- functions.php
+++ functions.php
@@ -8,7 +8,8 @@
  */
 
 /**
- * Set theme's version number. This is handy for cache busting on styles and scripts.
+ * Set theme's version number.
+ * This is handy for cache busting styles and scripts.
  */
 define( 'HERMI_VERSION', '0.1.0' );
 
@@ -16,58 +17,64 @@ define( 'HERMI_VERSION', '0.1.0' );
  * Include files that handle various functionality.
  */
 
-// Utility functions.
+// Load dependencies managed by Composer
+//require_once( get_template_directory() . '/includes/vendor/autoload.php' );
+
+// Handle recommended plugins functionality via TGM Plugin Activation 
+//require_once( get_template_directory() . '/includes/recommended-plugins.php' );
+
+// Utility functions
 require_once( get_template_directory() . '/includes/utilities.php' );
 
-// Helper functions.
+// Helper functions
 require_once( get_template_directory() . '/includes/helpers.php' );
 
-// Make the theme availble for translation.
+// Make the theme availble for translation
 require_once( get_template_directory() . '/includes/translation.php' );
 
-// Features added via add_theme_support() (mostly) are located here.
+// Features added via add_theme_support() (mostly) are located here
 require_once( get_template_directory() . '/includes/theme-support.php' );
 
-// Load JavaScript.
+// Load JavaScript
 require_once( get_template_directory() . '/includes/scripts.php' );
 
-// Load styles.
+// Load styles
 require_once( get_template_directory() . '/includes/styles.php' );
 
-// Wire up theme template hooks.
+// Wire up theme template hooks
 require_once( get_template_directory() . '/includes/theme-template-hooks.php' );
 
-// Functions used for generating output for theme templates.
+// Functions used for generating output for theme templates
 require_once( get_template_directory() . '/includes/theme-template-functions.php' );
 
-// Menus, menu walkers, menu set up.
+// Menus, menu walkers, menu set up
 require_once( get_template_directory() . '/includes/menus.php' );
 
 // Register sidebars and widget areas
 require_once( get_template_directory() . '/includes/widget-areas.php' );
 
-// Post hooks and functions.
+// Post hooks and functions
 require_once( get_template_directory() . '/includes/posts.php' );
 
-// Attachment hooks and functions.
+// Attachment hooks and functions
 require_once( get_template_directory() . '/includes/attachments.php' );
 
-// Comment hooks and functions.
+// Comment hooks and functions
 require_once( get_template_directory() . '/includes/comments.php' );
 
-// Pagination hooks and functions.
+// Pagination hooks and functions
 require_once( get_template_directory() . '/includes/pagination.php' );
 
-// Handle TinyMCE styling.
+// Handle TinyMCE styling
 require_once( get_template_directory() . '/includes/editor-style.php' );
 
-// Handle theme customization via the Theme Customizer.
+// Handle theme customization via the Theme Customizer
 require_once( get_template_directory() . '/includes/theme-customizer.php' );
 
-// Customize the WordPress log in page.
+// Customize the WordPress log in page
 require_once( get_template_directory() . '/includes/log-in.php' );
 
-// Functionality related to the custom post type demo templates.
+// Custom post type demo templates
 // Enable this for demo and testing purposes only. Custom Post Type and
 // Taxonomy registration should be handled by plugins.
  require_once( get_template_directory() . '/includes/custom-post-type-demo.php' );
