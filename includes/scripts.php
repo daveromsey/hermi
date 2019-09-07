85c52fdf09da9f69d993a745dbfae5400cb845c7
David Romsey <dromsey@gmail.com>
2019-09-06 21:43:49 -0400

    Fix inline docs.

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * Enqueue JavaScipt used on the front end of the theme.
- * 
+ * Enqueue JavaScript used on the front end of the theme.
+ *
  * JS is compiled via Gulp.
- * 
- * @since Hermi 0.1.0
+ *
+ * @since Hermi 1.0.0
  * @return void
  */
 add_action( 'wp_enqueue_scripts', 'hermi_scripts' );
 function hermi_scripts() {
 	// Add Foundation JavaScript to the footer.
-	wp_enqueue_script( 'foundation-js', 
+	wp_enqueue_script( 'foundation-js',
 		get_template_directory_uri() . '/assets/dist/js/foundation' . hermi_get_script_suffix() . '.js',
 		[ 'jquery' ],
 		HERMI_VERSION,
 		true
 	);
-	
+
 	// Add the rest of the theme's JavaScript to the footer.
 	// Note that scripts.js is generated using the theme's build process.
 	// See /build/config.yml: JAVASCRIPT_QUEUE
@@ -27,7 +27,7 @@ function hermi_scripts() {
 		HERMI_VERSION,
 		true
 	);
-	
+
 	// Load comment reply JavaScript only when necessary.
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) && ! is_front_page() ) {
 		wp_enqueue_script( 'comment-reply' );
